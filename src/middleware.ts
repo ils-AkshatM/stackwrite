5bf10703c9fa909ba6282502bc7467ca24736440
akshatmaheshwari <a.maheshwari@thesynapses.com>
2024-07-31 17:14:39 +0530

    Auth Setup

---
+++ src/middleware.ts
@@ -0,0 +1,28 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+import getOrCreateDB from './models/server/dbSetup';
+import getOrCreateStorage from './models/server/storageSetup';
+
+ 
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    await Promise.all([
+        getOrCreateDB(),
+        getOrCreateStorage(),
+    ])
+    return NextResponse.next()
+}
+ 
+// See "Matching Paths" below to learn more
+export const config = {
+    /* m atch all req paths except for ones that starts with: 
+    - api
+    - _next/static
+    - _next/image
+    - favicon.com
+    */
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+} 
